a6196c2540b02cde525b90b29a95049c6ce33e90
adesh.exc <bodead@rknec.edu>
2024-05-27 00:16:30 -0700

    Update firebase-config.js

--- client/src/utils/firebase-config.js
+++ client/src/utils/firebase-config.js
@@ -3,11 +3,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
 import { getAnalytics } from "firebase/analytics";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyC7ZxyzZr4vEsEMqp7N4stn_Xu4sJtnR0s",
   authDomain: "react-netflix-clone-655b0.firebaseapp.com",
