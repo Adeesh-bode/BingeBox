ba27814c2278d602fc66c75102a9134aea227560
adesh.exc <bodead@rknec.edu>
2024-05-27 00:15:41 -0700

    Update Context.js

--- client/src/utils/Context.js
+++ client/src/utils/Context.js
@@ -1,12 +1,1 @@
-// import React from 'react'
-// import { createContext } from 'react';
 
-// export const Context = createContext();
-
-// export default function AppContext({children}) {
-//   return (
-//     <Context.Provider value="" >
-//         { children }
-//     </Context.Provider>
-//   )
-// }
